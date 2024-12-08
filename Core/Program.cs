30de599dc075cab55bead3a0be3652c139fc91e4
Maloy <43298852+Ukinee@users.noreply.github.com>
2024-12-08 22:04:38 +0300

    Use nugets instead of submodules

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.Extensions.VectorData;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Memory;
+using Sume.Common.Configs;
 
 namespace Core
 {
     public class Program
     {
+        [Experimental("SKEXP0070")]
         public static void Main(string[] args)
         {
             // IVectorStore vectorStore = null;
 
             IKernelBuilder builder = Kernel.CreateBuilder();
 
-            builder.ConfigureServices();
+            builder.ConfigureServices(OllamaConfigs.GenerationConfig, OllamaConfigs.EmbeddingConfig);
         }
     }
 }
--- Core/Startup.cs
+++ Core/Startup.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics.CodeAnalysis;
+using Jabba.Ai.Generation.Setup;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
+using OllamaSharp;
 
 namespace Core
 {
     public static class Startup
     {
-        public static IKernelBuilder ConfigureServices(this IKernelBuilder kernelBuilder)
+        [Experimental("SKEXP0070")]
+        public static IKernelBuilder ConfigureServices
+        (
+            this IKernelBuilder kernelBuilder,
+            OllamaApiClient.Configuration chatConfig,
+            OllamaApiClient.Configuration embeddingConfig
+        )
         {
             kernelBuilder
+                .SetupGeneration(chatConfig, embeddingConfig)
                 .Services
                 .AddLogging()
                 ;
